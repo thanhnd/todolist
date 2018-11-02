14a75a736467fa39807a2ded09d9fcabded1fa7f
Thanh Nguyen <thanhssvn@gmail.com>
2018-11-02 18:50:55 +0700

    Spit components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Panel from './components/controls/Panel'
+import TaskList from './components/tasklist/TaskList'
+import Modal from './components/modals/Modal'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <h1 className="text-center my-2">QUẢN LÝ CÔNG VIỆC</h1>
+                    <div className="container-fluid">
+                        <div className="row">
+                            {/* PANEL */}
+                            <Panel />
+
+                            {/* DISPLAY */}
+                            <TaskList />
+                        </div>
+                    </div>
+                    
+                    {/* The Modal */}
+                    <Modal />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/controls/Panel.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AddNewTask from './AddNewTask';
+import FilterProgress from './FilterProgress';
+import FilterLabel from './FilterLabel';
+import FilterPriority from './FilterPriority';
+import Sort from './Sort';
+
+class Panel extends Component {
+    render() {
+        return (
+            <div className="col-md-3 text-center px-0">
+                <div className="header header--left d-flex align-items-center">
+                    <img src="./img/user_1.jpg" className="ml-2 user" alt="avatar" />
+                    <h3 className="text-white d-inline font-weight-light ml-2">Lê Quang Song</h3>
+                </div>
+                <AddNewTask />
+                {/* Filter */}
+                <div className="px-3">
+                    <FilterProgress />
+                    <FilterLabel />
+                    <FilterPriority />
+                    <Sort />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Panel;
